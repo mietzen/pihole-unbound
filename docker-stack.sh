21d01fd29baa72c3a902be34fd67ae86b740f97d
Nils Gerke <social.nge@mailbox.org>
2020-05-01 21:59:17 +0200

    test macvlan

--- docker-stack.sh
+++ docker-stack.sh
@@ -5,7 +5,12 @@ deploy () {
     docker network create -d bridge pihole_net \
         --subnet=10.0.0.0/29
 
-    echo "Create unbound"
+    echo "Create pihole_external"
+    docker network create -d macvlan pihole_external \
+        --subnet=10.19.89.0/24
+        --gateway=10.19.89.1
+
+    echo "Run unbound"
     docker run -d \
         --name unbound \
         -v $(pwd)/unbound/unbound.conf:/opt/unbound/etc/unbound/unbound.conf:ro \
@@ -14,27 +19,27 @@ deploy () {
         mvance/unbound:1.10.0
 
     echo "Create pihole"
-    docker run -d \
+    docker create \
         --name pihole \
-        -p 8080:80 \
-        -p 8443:443 \
-        -p 8053:53/tcp -p 8053:53/udp \
+        -p 80:80 \
+        -p 443:443 \
+        -p 53:53/tcp -p 53:53/udp \
         -e TZ="Berlin/Europe" \
         -e DNS1="10.0.0.2#53" \
         -e DNS2="no" \
-        -e ServerIP=$(uci show network.lan.ipaddr | cut -d'=' -f2) \
-        -e CONDITIONAL_FORWARDING=True \
-        -e CONDITIONAL_FORWARDING_IP=$(uci show network.lan.ipaddr | cut -d'=' -f2) \
-        -e CONDITIONAL_FORWARDING_DOMAIN=$(uci show dhcp.@dnsmasq[0].domain | cut -d'=' -f2) \
+        -e ServerIP=$(uci show network.lan.ipaddr | cut -d'=' -f2 | tr -d "'") \
         -v $(pwd)/pihole/etc-pihole/:/etc/pihole/ \
         -v $(pwd)/pihole/etc-dnsmasq.d/:/etc/dnsmasq.d/ \
         -v $(pwd)/pihole/01-pihole.conf:/etc/.pihole/advanced/01-pihole.conf:ro \
         --dns=127.0.0.1 --dns=10.64.0.1 \
-        --network=pihole_net --ip=10.0.0.3 \
+        --network=pihole_external --ip=10.19.89.3 \
         --restart=unless-stopped \
         --cap-add=NET_ADMIN \
         --link unbound \
         pihole/pihole:v4.4
+
+    docker network connect --ip 10.0.0.3 pihole_net pihole
+    docker start pihole
 }
 
 remove () {
